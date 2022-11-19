f9e96825f30af2567c4b23f33c877192130be845
zahrakaabi <kaabizahra@gmail.com>
2022-11-19 17:18:30 +0100

    chore: Prepare project env

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/* ---------------------------------------- */
+/*                DEPENDENCIES              */  
+/* ---------------------------------------- */
+// Packages
+
+// Styles
+import './shared/styles/global.css';
+
+/* ---------------------------------------- */
+/*                    APP                   */  
+/* ---------------------------------------- */
+function App() {
+  /* ************** RENDERING ************* */
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
