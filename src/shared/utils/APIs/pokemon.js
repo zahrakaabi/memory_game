2906fdeb05fdcf61ccb583929832eff4843f7db7
zahrakaabi <kaabizahra@gmail.com>
2022-11-19 18:13:17 +0100

    chore: Implement pokemon fake api

---
+++ src/shared/utils/APIs/pokemon.js
@@ -0,0 +1,64 @@
+/* ----------------------------------------- */
+/*                DEPENDENCIES               */
+/* ----------------------------------------- */
+// Images
+import PIKATCHU from '../../../assets/images/pikatchu.jpeg';
+import BLUE_TORTUE from '../../../assets/images/blue_tortue.jpeg';
+import BULBASAUR from '../../../assets/images/bulbasaur.png'; 
+import CHIBI_DEX from '../../../assets/images/chibiDex.jpeg';  
+import SNORLAX from '../../../assets/images/snorlax.jpeg';   
+import OTAKUKAN from '../../../assets/images/otakukan.png'; 
+
+/* ----------------------------------------- */
+/*                POKEMON API                */
+/* ----------------------------------------- */
+export const POKEMON_API = [
+    {
+        id: 0,
+        image: PIKATCHU,
+    },
+    {
+        id: 1,
+        image: BULBASAUR,
+    },
+    {
+        id: 2,
+        image: BLUE_TORTUE,
+    },
+    {
+        id: 3,
+        image: CHIBI_DEX,
+    },
+    {
+        id: 4,
+        image: OTAKUKAN,
+    },
+    {
+        id: 5,
+        image: SNORLAX,
+    },
+    {
+        id: 6,
+        image: PIKATCHU,
+    },
+    {
+        id: 7,
+        image: OTAKUKAN,
+    },
+    {
+        id: 8,
+        image: BLUE_TORTUE,
+    },
+    {
+        id: 9,
+        image: BULBASAUR,
+    },
+    {
+        id: 10,
+        image: SNORLAX,
+    },
+    {
+        id: 11,
+        image: CHIBI_DEX,
+    }
+]
