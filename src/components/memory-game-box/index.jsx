2d150d9eaddba829cf77814fdbc96ecb868f7f57
zahrakaabi <kaabizahra@gmail.com>
2022-11-22 16:11:41 +0100

    chore: Implement status changes + restart if game ends

--- src/components/memory-game-box/index.jsx
+++ src/components/memory-game-box/index.jsx
@@ -2,6 +2,7 @@
 /*              DEPENDENCIES           */
 /* ----------------------------------- */
 // Packages
+import { useState } from 'react';
 
 // Fake APIs
 import { POKEMON_API } from '../../shared/utils/APIs/pokemon';
@@ -13,18 +14,66 @@ import './index.css';
 /*             MemoryGameBox           */
 /* ----------------------------------- */
 function MemoryGameBox() {
+    // STATES
+    const [items, setItems] = useState([...POKEMON_API].sort(() => Math.random() - 0.5));
+    const [previous, setPrevious] = useState(-1);
+
+    // STATUS OF THE CARDS
+    const IsDuplicate  = (currentId) => {
+        if (items[currentId].image === items[previous].image) {
+            items[currentId].status = 'clicked-pokemon';
+            items[previous].status = 'clicked-pokemon';
+            setItems([...items]);
+            setPrevious(-1);
+        } else {
+            items[currentId].status = 'clicked-pokemon';
+            items[previous].status = 'clicked-pokemon';
+            setItems([...items])
+            setTimeout(() => {
+                items[currentId].status = '';
+                items[previous].status = '';
+                setItems([...items]);
+                setPrevious(-1);
+            }, 1000)
+        }
+    }
+
+    // HANDLE CLICK
+    const HandleClick = (id) => {
+        if (previous === -1) {
+            items[id].status = "active"
+            setItems([...items])
+            setPrevious(id)
+        } else {
+            IsDuplicate(id)
+        }
+    }
+
+    // RESTART GAME
+    const ReStartGame = items.every((item) => item.status)
+
     /* ********* RENDERING *********** */
     return (
-        <div className="memory-game-box">
-            {POKEMON_API.map((pokemon) => (
-                <div className="pokemon-box">
-                    <div className="pokemon pokemon-box_front" />
-                    <div className="pokemon pokemon-box_back">
-                        <img src={pokemon.image} alt='pokemon_img' />
-                    </div>
-                </div>
-            ))}
-        </div>
+        <>
+            <div className="memory-game-box">
+                {items.map((pokemon) => {
+                    const { status, id, image } = pokemon
+                    return (
+                        <div id="pokemon-box" 
+                            className={`pokemon-box ${status ? 'clicked-pokemon' : ''}`}
+                            key={id}
+                            onClick={() => HandleClick(items.findIndex((object) => object.id === id))}
+                        >
+                            <div className="pokemon pokemon-box_front" />
+                            <div className='pokemon pokemon-box_back'>
+                                <img src={image} alt='pokemon_img' />
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+            {ReStartGame && <div className="restart-game text-center" onClick={() => window.location.reload()}> Restart Game</div>}
+        </>
     )
 }
 
--- src/shared/utils/APIs/pokemon.js
+++ src/shared/utils/APIs/pokemon.js
@@ -16,49 +16,61 @@ export const POKEMON_API = [
     {
         id: 0,
         image: PIKATCHU,
+        status: '',
     },
     {
         id: 1,
         image: BULBASAUR,
+        status: '',
     },
     {
         id: 2,
         image: BLUE_TORTUE,
+        status: '',
     },
     {
         id: 3,
         image: CHIBI_DEX,
+        status: '',
     },
     {
         id: 4,
         image: OTAKUKAN,
+        status: '',
     },
     {
         id: 5,
         image: SNORLAX,
+        status: '',
     },
     {
         id: 6,
         image: PIKATCHU,
+        status: '',
     },
     {
         id: 7,
         image: OTAKUKAN,
+        status: '',
     },
     {
         id: 8,
         image: BLUE_TORTUE,
+        status: '',
     },
     {
         id: 9,
         image: BULBASAUR,
+        status: '',
     },
     {
         id: 10,
         image: SNORLAX,
+        status: '',
     },
     {
         id: 11,
         image: CHIBI_DEX,
+        status: '',
     }
 ]
